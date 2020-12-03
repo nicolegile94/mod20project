342b9d2863f8513e28288744a00f33b69c1fd7a5
Nicole Gile <nicolegile94@gmail.com>
2020-12-02 21:05:08 -0600

    initial about me page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+  const categories = [
+    {
+     name: "About Me",
+     description: "Information about Nicole"
+    },
+    {
+        name: "Portfolio",
+        description: "Sample of Nicole's work"
+    },
+    {
+        name: "Contact",
+        description: "How to reach Nicole"
+    },
+    {
+        name: "Resume",
+        description: "Resume for Nicole"
+    }
+ ];
+
+ const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+
+
   return (
     <div>
-      <Header>
-        <Nav></Nav>
-      </Header>
+        <Nav
+          categories={categories}
+          setCurrentCategory={setCurrentCategory}
+          currentCategory={currentCategory}
+        ></Nav>
       <main>
         <About></About>
       </main>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
-function Nav() {
+
+function Nav(props) {
+    const {
+      categories = [],
+      setCurrentCategory,
+      currentCategory,
+    } = props;
+
     return (
-        <nav>
-            <ul className="flex-row">
-                <li>
-                    <a href="#about">
-                        About me
-                    </a>
-                </li>
-                <li>
-                    <span>Contact</span>
-                </li>
+            <nav>
+                <ul className="flex-row">
+                    <li>
+                        <a href="#about">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                        {categories.map((category) => (
+                            <li
+                            className={`mx-1 ${
+                                currentCategory.name === category.name && 'navActive'
+                                }`}
+                              key={category.name}
+                        >
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category)
+                                }}
+                            >
+                            {category.name}
+                            </span>
+                    </li>
+                ))}
             </ul>
         </nav>
-    );
-}
+)};
 
 export default Nav;
